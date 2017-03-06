2c93e6989533e5f91d6468b123193c8c7b99b1bd
Scotimus <bakewells5856@gmail.com>
2017-03-06 17:00:53 +1300

    fix bug with removing roster events

--- AimyRoster/Script/RosterEvents.js
+++ AimyRoster/Script/RosterEvents.js
@@ -1,93 +1,26 @@
 ﻿$(function () {
-
-    $(".glyphicon-plus").on({
-        hover: function () {
-            $(this).css("color", "yellow");
-            $(this).css('cursor', 'pointer');
-            //$(this).animate({fontSize:"24px"},20);
-            $('.myClass').css('cursor', 'pointer');
-        },
-	    mouseleave: function () {
-	        $(this).css("color", "#32C0C6");
-	        $(this).css('cursor', 'auto');
-	        //$(this).animate({fontSize:"14px"},20);
-	}
-	});
-
-
-
-
-
-    $("body").on("click",".glyphicon-plus",function () {
-
+    //Add Event
+    $(".glyphicon-plus").click(function () {
         var startTime = $('#startTime').val();
-        if (!startTime)
-            startTime = '6:00 AM';
         var finishTime = $('#finishTime').val();
-        if (!finishTime)
-            finishTime = '12:00 PM';
 
-        var div =
+        if (finishTime || startTime) {
+            var div =
             $("<div class='innerdiv'>" + startTime + " - " + finishTime + "<span class='glyphicon glyphicon-remove'></div>");
-        $(this).before(div);
+            $(this).before(div);
+        }
+        else {
+            window.alert("Please select a time");
+        }
 
-        //console.log($('#sites').val());
-        //var siteName = $('#sites').val()
-
-        ////console.log($(this).parent().parent().prev().children().first().text());
-        //var staffName = $(this).parent().parent().prev().children().first().text();
-        //var sDate = $(this).parent().attr('class');
-        //var sDateArray = sDate.split(" ");
-        //var dateJson = $("#dateHeader").children().get(sDateArray[1].charAt(1) - 1).innerHTML;
-
-        //var tableIndex = sDateArray[1] + sDateArray[2];
-        //var scheduleData = { site: siteName, staff: staffName, scheduleDate: dateJson, startHour: startTime, finishHour: finishTime, index: tableIndex };
-        //console.log(JSON.stringify(scheduleData) + ":testing 1");
-        //scheduleArray.push(scheduleData);
     });
 
 
 
 
-
-    $(".glyphicon-remove").on({
-        hover: function () {
-            $(this).css('cursor', 'pointer');
-        },
-        mouseleave: function () {
-            $(this).css('cursor', 'auto');
-        }
-	});
-
-
-
-
-
+    //remove Event
     $("body").on("click", ".glyphicon-remove", function (event) {
-       // var hoursArray = $(this).parent().text().split("-");
 
-       // var startTime = hoursArray[0].slice(0, -1);
-       // console.log(startTime);
-       // var finishTime = hoursArray[1].slice(1);
-       // console.log(finishTime);
-       // var staffName = $(this).parent().parent().parent().prev().children().first().text();
-       // var siteName = $('#sites').val()
-
-       // var sDate = $(this).parent().parent().attr('class');
-       // var sDateArray = sDate.split(" ");
-       //// var dateJson = $("#dateHeader").children().get(sDateArray[1].charAt(1) - 1).innerHTML;
-
-       //// var scheduleData = { site: siteName, staff: staffName, scheduleDate: dateJson, startHour: startTime, finishHour: finishTime };
-
-       // var tableIndex = sDateArray[1] + sDateArray[2];
-
-       // console.log(JSON.stringify(scheduleData) + ":testing 2");
-        //console.log(scheduleArray.indexOf(scheduleData))
-
-       /* var indexInt = scheduleArray.findIndex(
-			x => x.index == tableIndex
-		);
-        console.log(indexInt);*/
         $(this).parent().remove();
 
     });
@@ -97,6 +30,4 @@
     $("#reference").kendoDropDownList();
     $("#add").kendoButton();
 
-
 });
-
